80efaed704e4c43412566cfd0aed92cf30aaa911
xqadx101685-boop <xqa.dx.101685@gmail.com>
2026-06-30 09:05:17 +0900

    Update comment_scraper.py

--- comment_scraper.py
+++ comment_scraper.py
@@ -204,6 +204,8 @@ def run_comment_collection(gc: gspread.Client, source_sheet_id: str, source_shee
                 is_target, max_limit = True, 1500
             elif target_company.startswith("三菱") and item['count'] >= 100:
                 is_target, max_limit = True, 1500
+            elif target_company.startswith("ダイハツ") and item['count'] >= 100:
+                is_target, max_limit = True, 1500
             elif str(nissan_neg_text).strip() not in ["", "なし", "N/A", "-"]:
                 is_target, max_limit = True, 1500
         
